b23c8ea5b1f09b29c8007e117c336881f64d05eb
sarvagya-ui <sarvagya.pandey@outlook.com>
2021-08-23 15:59:02 +0530

    till formEvent

--- src/app/observable/from-event/from-event.component.ts
+++ src/app/observable/from-event/from-event.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { DesignUtilityService } from '../../appServices/design-utility.service';
 
 @Component({
   selector: 'app-from-event',
   templateUrl: './from-event.component.html',
   styleUrls: ['./from-event.component.css']
 })
-export class FromEventComponent implements OnInit {
+export class FromEventComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private _designUtilityService: DesignUtilityService) { }
+  count: number = 1;
+
+  @ViewChild('addBtn') addBtn: ElementRef;
 
   ngOnInit(): void {
+  } 
+
+  ngAfterViewInit() {
+
+    fromEvent(this.addBtn.nativeElement, 'click').subscribe(res => {
+      let countVal = 'video ' + this.count++;
+      console.log(countVal);
+      this._designUtilityService.print(countVal, 'elContainer');
+      this._designUtilityService.print(countVal, 'elContainer2');
+    });
   }
+
 
 }

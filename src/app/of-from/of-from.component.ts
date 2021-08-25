1eae6947b3499c89b434cdece631ec6aae6b4736
sarvagya-ui <sarvagya.pandey@outlook.com>
2021-08-25 16:17:21 +0530

    till of and from operator

--- src/app/of-from/of-from.component.ts
+++ src/app/of-from/of-from.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { DesignUtilityService } from '../appServices/design-utility.service';
 
 @Component({
   selector: 'app-of-from',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfFromComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _designUtilityService: DesignUtilityService) { }
+  obsmsg: any;
   ngOnInit(): void {
+    //of
+    const obs1 = of('anupam', 'sam', 'ram');
+    obs1.subscribe(res =>
+      this._designUtilityService.print(res, 'elContainer')
+    );
+
+    const obs2 = of({ a: 'anupam', b: 'sam', c: 'ram' });
+ obs2.subscribe(res =>
+      this.obsmsg = res
+    );
+
+    //from
+    const obs3 = from(['anupam','sam','ram']);
+    obs3.subscribe(res=>
+      this._designUtilityService.print(res,'elContainer3')
+    );
+
+    const obs4=from("hello");
+    obs4.subscribe(res=>{
+      this._designUtilityService.print(res,'elContainer4')
+    });
   }
 
 }
